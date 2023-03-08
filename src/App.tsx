22cfe7a8ed2f491f849ecada130bf3ca7c301b92
Ismoiljon (Jon) <abduraimovismoil98@gmail.com>
2023-03-08 12:10:22 +0900

    adding typescript types for data

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Greet from "./components/Greet";
 function App() {
   return (
     <div className="App">
-      <Greet name="Ismoiljon" />
+      <Greet name="Ismoiljon" messageCount={25} isLoggedIn={false} />
     </div>
   );
 }
--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,11 +1,17 @@
 type GreetProps = {
   name: string;
+  messageCount: number;
+  isLoggedIn: boolean;
 };
 
 const Greet = (props: GreetProps) => {
   return (
     <div>
-      <h1>Hello my name is {props.name}</h1>
+      <h1>
+        {props.isLoggedIn
+          ? `Hello my name is ${props.name}. I am ${props.messageCount} years old`
+          : `Welcome Guest`}
+      </h1>
     </div>
   );
 };
